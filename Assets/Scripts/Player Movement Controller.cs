5200241c40789c8b9c8d34b6c1499ea6b0b8c255
Kenny Lee <kenjlee09@gmail.com>
2025-07-16 15:46:54 -0700

    added player rotation and basic animations

--- Assets/Scripts/Player Movement Controller.cs
+++ Assets/Scripts/Player Movement Controller.cs
@@ -6,6 +6,7 @@ public class PlayerMovementController : MonoBehaviour
     public Transform playerTransform;
     public Rigidbody playerRB;
     public InputManager inputs;
+    public Animator playerAnim;
 
     [Header("Camera Components")]
     public Transform cameraTransform;
@@ -14,31 +15,57 @@ public class PlayerMovementController : MonoBehaviour
 
     public Transform playerOrientation;
 
+    [Header("Physics")]
+    public bool playerOnGround;
+    public bool playerMoving;
+
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
         inputs = GetComponent<InputManager>();
         playerTransform = GetComponent<Transform>();
+        playerAnim = GetComponent<Animator>();
 
         playerRB.constraints = RigidbodyConstraints.FreezeRotation;
     }
 
     void Update()
     {
-        setPlayerOrientation();
+        CheckPlayerStatus();
         Vector3 moveDirection = inputs.GetMovementInput();
+        SetPlayerAnimation();
+        SetPlayerOrientation(moveDirection);
         MovePlayer(moveDirection);
     }
 
-    void setPlayerOrientation()
+    void SetPlayerOrientation(Vector3 moveDir)
     {
         Vector3 rotationValues = playerTransform.position - new Vector3(cameraTransform.position.x, playerTransform.position.y, cameraTransform.position.z);
+        Vector3 moveOrientation = new Vector3(playerRB.linearVelocity.normalized.x, 0, playerRB.linearVelocity.normalized.z);
         playerOrientation.forward = rotationValues.normalized;
+        playerTransform.forward = moveOrientation.magnitude > 0 ? moveOrientation : playerTransform.forward;   
+    }
+
+    void SetPlayerAnimation()
+    {
+        if (playerMoving)
+        {
+            playerAnim.Play("Walking");
+        }
+        else
+        {
+            playerAnim.Play("Standing");
+        }
     }
 
     public void MovePlayer(Vector3 moveDir)
     {
         Vector3 moveForce = (moveDir.z * playerOrientation.forward + moveDir.x * playerOrientation.right).normalized * playerSpd;
         playerRB.AddForce(moveForce, ForceMode.Force);
+    }
+
+    public void CheckPlayerStatus()
+    {
+        playerMoving = playerRB.linearVelocity.magnitude > 3f;
     }
 }
